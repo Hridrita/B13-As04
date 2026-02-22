33f7bb1fa60507b16d3b0f0bcaf41537df7670c3
WT_FALL-25-26- <moonsaha721@gmail.com>
2026-02-22 22:12:17 +0600

    added not applied button logic

---
+++ logic.js
@@ -0,0 +1,40 @@
+const interviewButtons = document.querySelectorAll('.interview-btn');
+const rejectedButtons = document.querySelectorAll('.rejected-btn');
+
+
+interviewButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        //1. sob gula card er not applied button k access krbo
+        const card = this.parentElement; 
+        const appliedBtn = card.querySelector('.applied-btn');
+
+        //2. button e interview lekha uthbe
+        
+        appliedBtn.innerText = "INTERVIEWED";
+
+        //3. applied button ta remove krbo
+
+        
+        appliedBtn.classList.remove('btn-primary', 'text-[#002c5cFF]', 'bg-red-500', 'btn-error');
+
+        //4. interview button er jnno bg color r text color dib
+        
+        
+        appliedBtn.classList.add('btn-success', 'text-white', 'bg-green-500');
+    });
+});
+
+rejectedButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        const card = this.parentElement;
+        const appliedBtn  = card.querySelector('.applied-btn');
+        
+        appliedBtn.innerText = "REJECTED";
+
+        
+        appliedBtn.classList.remove('btn-primary', 'text-[#002c5cFF]', 'bg-green-500', 'btn-success');
+        
+        
+        appliedBtn.classList.add('btn-error', 'text-white', 'bg-red-500');
+    });
+});
